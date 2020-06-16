191542f2cdc177a327d90605dfea315cb14ea36f
Jose Chañi Laura <jchanila@unsa.edu.pe>
2020-06-16 07:02:30 -0500

    funciones iniciales

--- app/src/main/java/com/example/dnpa_sensorproyect/view/MainActivity.java
+++ app/src/main/java/com/example/dnpa_sensorproyect/view/MainActivity.java
@@ -1,16 +1,59 @@
 package com.example.dnpa_sensorproyect.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.dnpa_sensorproyect.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        initGPS();
+
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d(TAG, "Location not allowed");
+                }
+                return;
+            }
+        }
+    }
+
+    private void initAcelerometer(){
+
+    }
+
+    private void initGPS(){
+
     }
 }
