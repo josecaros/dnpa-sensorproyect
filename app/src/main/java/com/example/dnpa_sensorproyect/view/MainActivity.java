eeb207bc6432307afa22c87ecdb0cf3258c42606
Jose Chañi Laura <jchanila@unsa.edu.pe>
2020-06-16 06:51:17 -0500

    Vistas iniciales

--- app/src/main/java/com/example/dnpa_sensorproyect/view/MainActivity.java
+++ app/src/main/java/com/example/dnpa_sensorproyect/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dnpa_sensorproyect;
+package com.example.dnpa_sensorproyect.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dnpa_sensorproyect.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/dnpa_sensorproyect/view/VistaInicial.java
@@ -0,0 +1,14 @@
+package com.example.dnpa_sensorproyect.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.dnpa_sensorproyect.R;
+
+public class VistaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_inicial);
+    }
+}
